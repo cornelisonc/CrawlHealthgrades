1a501ddd8a53c8bff9cb0e007eecdb1485128062
Ceili Cornelison <ceili@ceilicornelison.com>
2013-11-06 11:20:47 -0600

    Gitignore compiled python

--- healthgrades/spiders/healthgrades_spider.py
+++ healthgrades/spiders/healthgrades_spider.py
@@ -1,12 +1,40 @@
+from selenium import selenium
+from selenium.selenium import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
 from scrapy.spider import BaseSpider
+from scrapy.selector import HtmlXPathSelector
+from scrapy.http import Request, FormRequest
+from healthgrades.items import HealthgradesItem
+import time
+import datetime
+import re
+import string
 
 class HealthgradesSpider(BaseSpider):
-	name = "healthgrades"
-	allowed_domains = ["healthgrades.com"]
-	start_urls = [
-		"http://www.healthgrades.com/find-a-doctor"
-	]
+    name = "healthgrades"
+    allowed_domains = ["healthgrades.com"]
+    start_urls = [
+        "http://www.healthgrades.com/find-a-doctor"
+    ]
 
-	def parse(self, response):
-		filename = response.url.split("/")[-2]
-		open(filename, 'wb').write(response.body)
+    def __init__(self, **kwargs):
+        self.driver = webdriver.Firefox()
+        driver = self.driver
+        driver.implicitly_wait(30)
+        driver.get("http://www.healthgrades.com/find-a-doctor")
+
+        state_form_entry = driver.find_element_by_id('multi_search_location_textbox')
+        state_form_entry.click()
+        state_form_entry.send_keys("Missouri")
+
+        driver.find_element_by_xpath("//span[@class='hgSearchTable']/span[@class='hgSearchTableRow']/button[@class='buttonHeaderSearch']").click()
+        
+        super(HealthgradesSpider, self).__init__()
+
+    def parse(self, response):
+        filename = response.url.split("/")[-2]
+        open(filename, 'wb').write(response.body)
